a2cdd10e7ecedee2b6aa86385f222738ca310559
sevan96 <sevanakoumia@gmail.com>
2024-04-26 16:32:54 +0000

    Création du composant Tableau qui affiche la liste des bénéficiaires d'un marchand

---
+++ components/dashboard/send-money/BeneficiariesTable.tsx
@@ -0,0 +1,150 @@
+"use client"
+
+import AccountListAndTransactionsProps from "@/components/dashboard/send-money/AccountListAndTransactions";
+import React, {useState, useEffect} from "react";
+import { type ColumnDef } from "@tanstack/react-table"
+import {
+    filterableColumns,
+    getColumns,
+    searchableColumns
+} from "@/components/dashboard/send-money/beneficiaries-table-columns";
+import {useDataTable} from "@/hooks/use-data-table";
+import {TDataTableBeneficiary} from "@/components/dashboard/send-money/data-table/DataTableBeneficiary";
+
+import { DateRange } from "react-day-picker"
+import { addDays, startOfYear, endOfDay, format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+import { IBeneficiary } from '@/core/interfaces/beneficiary';
+import {IUser} from "@/core/interfaces/user";
+
+interface BeneficiariesTableProps {
+    searchItems: {
+        per_page: number;
+        page: number;
+        search?: string;
+        account?: string;
+        from?: string;
+        sort?: string;
+        to?: string;
+        status?: string
+    },
+    lang: string,
+    selectedAccount: string,
+    beneficiaries: IBeneficiary[],
+}
+
+// export type BeneficiariesDataType = {
+//     id: string
+//     reference: string
+//     firstName: string
+//     lastName: string
+//     createdAt: string
+// }
+
+export default function BeneficiariesTable({ searchItems, lang, selectedAccount, beneficiaries }: BeneficiariesTableProps) {
+
+    const [pSearch, setPSearch] = useState(searchItems.search ?? '');
+    const [pStatus, setPStatus] = useState(searchItems.status ?? '');
+    const [date, setDate] = React.useState<DateRange | undefined>({
+        from: searchItems.from ? new Date(searchItems.from) : startOfYear(new Date()),
+        to: searchItems.to ? new Date(searchItems.to) : endOfDay(new Date()),
+    })
+
+    let data: IBeneficiary[] = [
+        // {
+        //     id: "1",
+        //     reference: "245653FS34S",
+        //     createdAt: "2024-04-20T11:00:00",
+        //     firstName: "Didier Aney",
+        //     lastName: "Didier Aney",
+        // },
+        // {
+        //     id: "2",
+        //     reference: "245653FS34S",
+        //     createdAt: "2023-04-20T11:00:00",
+        //     firstName: "Didier Aney",
+        //     lastName: "Didier Aney",
+        // },
+        // {
+        //     id: "3",
+        //     reference: "245653FS34S",
+        //     createdAt: "2024-02-20T08:00:00",
+        //     firstName: "Didier Aney",
+        //     lastName: "Didier Aney",
+        // },
+        // {
+        //     id: "4",
+        //     merchantId: "245653FS34S",
+        //     date: "2024-04-20T11:00:00",
+        //     amount: 3493774,
+        //     beneficiary: "Koffi Olivier",
+        //     account: "+225 07 73 44 11 00",
+        //     status: "declined"
+        // },
+        // {
+        //     id: "5",
+        //     merchantId: "245653FS34S",
+        //     date: "2024-01-20T11:00:00",
+        //     amount: 3493774,
+        //     beneficiary: "Didier Aney",
+        //     account: "+225 07 77 40 41 36",
+        //     status: "approved"
+        // },
+        // {
+        //     id: "6",
+        //     merchantId: "245653FS34S",
+        //     date: "2024-04-20T11:00:00",
+        //     amount: 3493774,
+        //     beneficiary: "Didier Aney",
+        //     account: "+225 07 77 40 41 36",
+        //     status: "approved"
+        // },
+        // {
+        //     id: "7",
+        //     merchantId: "245653FS34S",
+        //     date: "2024-04-20T11:00:00",
+        //     amount: 3493774,
+        //     beneficiary: "Didier Aney",
+        //     account: "+225 07 77 40 41 36",
+        //     status: "approved"
+        // }
+    ];
+    //data = beneficiaries as IBeneficiary;
+    const pageCount = 2;
+
+    const columns = React.useMemo<ColumnDef<IBeneficiary, unknown>[]>(
+        () => getColumns(lang),
+        []
+    )
+
+    const { table } = useDataTable({
+        data,
+        columns,
+        pageCount,
+        searchableColumns,
+        filterableColumns,
+        selectedAccount,
+        pSearch,
+        pStatus,
+        date
+    })
+
+    const id = React.useId()
+
+    return (
+        <div>
+            <TDataTableBeneficiary
+                table={table}
+                columns={columns}
+                selectedAccount={selectedAccount}
+                pSearch={pSearch}
+                setPSearch={setPSearch}
+                pStatus={pStatus}
+                setPStatus={setPStatus}
+                date={date}
+                setDate={setDate}
+                lang={lang}
+            />
+        </div>
+    );
+}
