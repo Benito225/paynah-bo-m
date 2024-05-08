e6b0ad95cc6d50c73e352f28208dbd8d1065328b
sevan96 <sevanakoumia@gmail.com>
2024-05-08 08:59:05 +0000

    Implémentation du service de liste des pays et liste des opérateurs par pays

---
+++ core/apis/country.ts
@@ -0,0 +1,19 @@
+"use server"
+// import {auth} from "@/auth";
+// import {IUser} from "@/core/interfaces/user";
+
+import {fetchData} from "@/lib/api";
+
+export async function getCountries(token: string) {
+    const resData = await fetchData(`/countries`, 'GET', null, token, true);
+    console.log(resData);
+    console.log(token);
+    return resData.data;
+}
+
+export async function getCountryOperators(countryId: string, token: string) {
+    const resData = await fetchData(`/countries/${countryId}/operators`, 'GET', null, token, true);
+    console.log(resData);
+    console.log(token);
+    return resData.data;
+}
