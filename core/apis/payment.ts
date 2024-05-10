f989d2be1233b182d15a47ece31f0a54d6f0cf06
sevan Akoumia <sevanakoumia@192.168.1.24>
2024-05-10 22:49:43 +0000

    Implémentation du service de payout

--- core/apis/payment.ts
+++ core/apis/payment.ts
@@ -5,6 +5,8 @@
 import {fetchData} from "@/lib/api";
 import { format } from "date-fns";
 
+const INIT_PAYOUT_DESC = "Envoi d'argent";
+
 export async function generateQuickPaymentLink(values: any, merchantId: string, token: string) {
     const data = {
         'bankAccountId': values.bankAccountId,
@@ -18,4 +20,40 @@ export async function generateQuickPaymentLink(values: any, merchantId: string, 
     };
 
     return await fetchData(`/merchants/${merchantId}/quick-payment-link`, 'POST', data, token, true);
+}
+
+export async function initPayout(values: any, merchantId: string, token: string) {
+    const data = {
+        'bankAccountId': values.bankAccountId,
+        'firstName': values.firstName,
+        'lastName': values.lastName,
+        'operator': values.operator,
+        'phoneNumber': values.phoneNumber,
+        'paynahAccount': values.paynahAccount,
+        'bankAccount': values.bankAccount,
+        'amount': values.amount,
+        'description': INIT_PAYOUT_DESC,
+        'mode': getPayoutModeSendToAPI(values.mode),
+        'feeSupport': false,
+    };
+    console.log(data)
+    return await fetchData(`/merchants/${merchantId}/quick-payout`, 'POST', data, token, true);
+}
+
+const getPayoutModeSendToAPI = (activeSendMode: string) => {
+    let mode = 0;
+    switch (activeSendMode) {
+        case "direct":
+            mode = 1
+            break;
+        case "mm":
+            mode = 2
+            break;
+        case "bank":
+            mode = 3
+            break;
+        default:
+            break;
+        }
+    return mode;
 }
