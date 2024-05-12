51b98e609d2807896bd680cd5aa60d87e4810edd
sevan Akoumia <sevanakoumia@192.168.1.2>
2024-05-12 00:09:48 +0000

    Implémentation du service d'envoi de lien via un canal

--- core/apis/payment.ts
+++ core/apis/payment.ts
@@ -3,9 +3,25 @@
 // import {IUser} from "@/core/interfaces/user";
 
 import {fetchData} from "@/lib/api";
-import { format } from "date-fns";
+import { format, formatISO } from "date-fns";
 
 const INIT_PAYOUT_DESC = "Envoi d'argent";
+
+export async function generatePaymentLinkToShare(values: any, merchantId: string, token: string) {
+    const data = {
+        'bankAccountId': values.bankAccountId,
+        'firstName': values.firstName,
+        'lastName': values.lastName,
+        'phoneNumber': "+2250759958400", // optional parameter
+        'email': values.email,
+        'amount': values.amount,
+        'motif': "Collecte de fonds",
+        'canal': getCanalToPaymentLink(values.phoneNumber, values.email),
+        'expirationDate': formatISO(new Date()),
+    };
+    console.log(data);
+    return await fetchData(`/merchants/${merchantId}/payment-link/generate`, 'POST', data, token, true);
+}
 
 export async function generateQuickPaymentLink(values: any, merchantId: string, token: string) {
     const data = {
@@ -55,5 +71,16 @@ const getPayoutModeSendToAPI = (activeSendMode: string) => {
         default:
             break;
         }
+    return mode;
+}
+
+const getCanalToPaymentLink = (phoneNumber: string, email: string) => {
+    let mode = 'SMS';
+    if (email.trim().length > 0) {
+        mode = 'EMAIL';
+    }
+    if (phoneNumber.trim().length > 0) {
+        mode = 'SMS';
+    }
     return mode;
 }
