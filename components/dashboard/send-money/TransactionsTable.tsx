2a82b50f1b3aef3de01e84decf2db6c26696a815
sevan Akoumia <sevanakoumia@MacBook-Pro-de-sevan.local>
2024-05-15 19:37:42 +0000

    Impémentation de recherche de transactions dans la liste par la clé searchTerm

--- components/dashboard/send-money/TransactionsTable.tsx
+++ components/dashboard/send-money/TransactionsTable.tsx
@@ -71,7 +71,7 @@ export default function TransactionsTable({ searchItems, lang, selectedAccount, 
     const endPeriod = new Date(query.to ?? "");
     const formatStartPeriod = startPeriod.toLocaleDateString('en-GB');
     const formatEndPeriod = endPeriod.toLocaleDateString('en-GB');
-    const url = `/transactions/all-transactions/with-filters?merchantId=${query.merchantId}&search=${query.search ?? ""}&status=${query.status ?? ""}&page=${query.page}&perPage=${query.perPage}&from=${formatStartPeriod}&to=${formatEndPeriod}&csv=false`;
+    const url = `/transactions/all-transactions/with-filters?merchantId=${query.merchantId}&searchTerm=${query.search ?? ""}&status=${query.status ?? ""}&page=${query.page}&perPage=${query.perPage}&from=${formatStartPeriod}&to=${formatEndPeriod}&csv=false`;
 
     useEffect(() => {
         // fecthTransactions("");
