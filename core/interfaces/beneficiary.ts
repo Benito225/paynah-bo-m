1902595058ddb255f56d06a522f5e6249207e992
sevan Akoumia <sevanakoumia@MacBook-Pro-de-sevan.local>
2024-05-31 14:21:22 +0000

    Implémentation de la fonctionnalité de liste des bénéficiaires sur la page envoi de lien de paiement

--- core/interfaces/beneficiary.ts
+++ core/interfaces/beneficiary.ts
@@ -1,9 +1,9 @@
 export interface IBeneficiary {
     id?: string,
     reference?: string,
-    firstName?: string,
-    lastName?: string,
-    email?: string,
+    firstName: string,
+    lastName: string,
+    email: string,
     createdAt?: Date,
     updatedAt?: Date,
     deletedAt?: Date,
