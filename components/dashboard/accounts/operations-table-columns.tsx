135a065a520a0c10d7d3303a51d1e3833e511688
sevan Akoumia <sevanakoumia@MacBook-Pro-de-sevan.local>
2024-06-13 16:52:47 +0000

    Retirer les options Faire une réclamation & Relancer l’opération

--- components/dashboard/accounts/operations-table-columns.tsx
+++ components/dashboard/accounts/operations-table-columns.tsx
@@ -156,7 +156,7 @@ export function getColumns(lang: string): ColumnDef<ITransaction>[] {
                                     <ClipboardList className="mr-2 h-3.5 w-3.5" />
                                     <span className={`mt-[1.5px]`}>{`Détails de l'opération`}</span>
                                 </DropdownMenuItem>
-                                <DropdownMenuSeparator />
+                                {/* <DropdownMenuSeparator />
                                 <DropdownMenuItem className={`text-xs cursor-pointer`}>
                                     <AlertTriangle className="mr-2 h-3.5 w-3.5" />
                                     <span className={`mt-[1.5px]`}>Faire une réclamation</span>
@@ -165,7 +165,7 @@ export function getColumns(lang: string): ColumnDef<ITransaction>[] {
                                 <DropdownMenuItem className={`text-xs cursor-pointer`}>
                                     <RotateCw className="mr-2 h-3.5 w-3.5" />
                                     <span className={`mt-[1.5px]`}>{`Refaire l'opération`}</span>
-                                </DropdownMenuItem>
+                                </DropdownMenuItem> */}
                             </DropdownMenuContent>
                         </DropdownMenu>
                     </div>
